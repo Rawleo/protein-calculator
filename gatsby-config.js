2afa363678d8d7ef2c820af8fe21997ae7d0a34c
Rawleo <sonryan50@gmail.com>
2026-01-13 20:25:36 -0600

    Adding favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,17 @@ module.exports = {
   pathPrefix: "/protein-calculator",
   plugins: [
     'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Daily Protein Planner`,
+        short_name: `Protein`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#4f46e5`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
   ],
 }
